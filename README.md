56daddf901ad91f25b5f8f7d1bd82a8f2efa95f5
Temechon <temechon@gmail.com>
2018-03-10 22:01:20 +0100

    Updated with animations - WIP

--- README.md
+++ README.md
@@ -16,35 +16,4 @@ npm install -g grunt
 npm run start
 ```
 That will run a webserver and open your default browser at https://localhost:3000. Validate the certificate (if asked in the browser), and go to the following URL
-- [DEV URL](https://www.facebook.com/embed/instantgames/231358994040500/player?game_url=https://localhost:3000)
-- [PROD URL](https://www.facebook.com/embed/instantgames/1246601822117867/player?game_url=https://localhost:3000)
-
-## Deploy in Messenger
-
-### 1. Build
-`CTRL+ù` to open the terminal in VSCode.
-Type `grunt` to create the package (named `1boss_YYYYMMDD-HHMMSS.zip`).
-
-### 3a. Deploy for Development
-
-* Be sure to have the correct API url in `ServerHerlper.ts`
-* Open the Facebook application page
-* Select the application `1BOSS - Dev`
-* Select `Web Hosting` on the left menu
-* Upload your package.
-* When the new version state is 'standby', click again on `Web Hosting`
-* Select your new version and click on the star `Test mode`.
-
-You're done!
-
-### 3b. Deploy for Production
-
-* Be sure to have the correct API url in `ServerHerlper.ts`
-* Open the Facebook application page
-* Select the application `1BOSS`
-* Select `Web Hosting` on the left menu
-* Upload your package.
-* When the new version state is 'standby', click again on `Web Hosting`
-* Select your new version and click on the star `Set in production`.
-
-You're done!
+- [URL](https://www.facebook.com/embed/instantgames/337333786786432/player?game_url=https://localhost:3000)
--- ts/Game.ts
+++ ts/Game.ts
@@ -3,6 +3,17 @@ class Game {
     private engine: BABYLON.Engine;
     public scene: BABYLON.Scene;
 
+    private _buttons: Array<BABYLON.AbstractMesh> = [];
+    private centerPosition: BABYLON.Vector3;
+
+    private sequence: Array<number> = [];
+    private runningSequenceIndex = 0;
+
+    private cameraParams = { alpha: -1.57, beta: 0.89 };
+
+    // True when the player can start to reproduce the sequence
+    private _waitForInput: boolean = false;
+
     constructor(canvasId: string) {
 
         let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById(canvasId);
@@ -18,12 +29,25 @@ class Game {
         this._initScene();
 
         let loader = new BABYLON.AssetsManager(this.scene);
-        loader.addMeshTask("simon", "", 'assets/', "simon.babylon");
+        loader.addMeshTask("simon", "", 'assets/', "simon.babylon").onSuccess = (task: BABYLON.MeshAssetTask) => {
+
+            // Get buttons
+            for (let obj of task.loadedMeshes) {
+                obj.isPickable = true;
+                if (obj.name.indexOf('button') !== -1) {
+                    this._buttons.push(obj);
+                }
+                if (obj.name.indexOf('socle') !== -1) {
+                    this.centerPosition = obj.position.clone();
+                }
+            }
+        }
 
         loader.onFinish = this._run.bind(this);
         loader.onProgress = (remaining: number, totalCount: number) => {
             FBInstant.setLoadingProgress(100 - remaining / totalCount);
-        }
+        };
+
 
         loader.load();
 
@@ -35,7 +59,7 @@ class Game {
 
         this.scene = new BABYLON.Scene(this.engine);
 
-        let camera = new BABYLON.ArcRotateCamera('', 0, 0, 5, BABYLON.Vector3.Zero(), this.scene);
+        let camera = new BABYLON.ArcRotateCamera('', 0, 0, 250, BABYLON.Vector3.Zero(), this.scene);
         camera.attachControl(this.engine.getRenderingCanvas());
         let light = new BABYLON.HemisphericLight('', new BABYLON.Vector3(0, 1, 0), this.scene);
 
@@ -59,5 +83,72 @@ class Game {
 
     private _initGame() {
 
+        this.scene.stopAllAnimations();
+
+        this.scene.debugLayer.show();
+        (this.scene.activeCamera as BABYLON.ArcRotateCamera).target = this.centerPosition;
+
+        let easing = new BABYLON.ExponentialEase(5);
+        easing.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+
+        // World extends
+        var worldExtends = this.scene.getWorldExtends();
+        var worldSize = worldExtends.max.subtract(worldExtends.min);
+        var worldCenter = worldExtends.min.add(worldSize.scale(0.5));
+        var radius = worldSize.length() * 1.25;
+
+        BABYLON.Animation.CreateAndStartAnimation('', this.scene.activeCamera, 'alpha', 60, 100, 0, this.cameraParams.alpha, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT, easing);
+        BABYLON.Animation.CreateAndStartAnimation('', this.scene.activeCamera, 'beta', 60, 100, 0, this.cameraParams.beta, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT, easing);
+        BABYLON.Animation.CreateAndStartAnimation('', this.scene.activeCamera, 'radius', 60, 100, 250, radius, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT, easing, this._startGame.bind(this));
+
+        this.scene.onPointerDown = (evt: PointerEvent, pickInfo: BABYLON.PickingInfo) => {
+            console.log(pickInfo.pickedMesh.name);
+            // if (this._waitForInput && pickInfo.hit) {
+            //     console.log(pickInfo.pickedMesh.name);
+            // }
+        }
     }
+
+    private _startGame() {
+        this._waitForInput = false;
+        this.runningSequenceIndex = 0;
+        this.sequence = [];
+
+        this._gameLoop().then(() => {
+            this._waitForInput = true;
+        });
+
+    }
+
+    private async _gameLoop() {
+        this._waitForInput = false;
+        this._addButton();
+
+        for (let i = 0; i < this.sequence.length; i++) {
+            await this._showAnimationWithSound();
+            this.runningSequenceIndex++;
+        }
+    }
+
+
+    private _showAnimationWithSound(): Promise<any> {
+        this.scene.stopAllAnimations();
+        return new Promise<any>((resolve, reject) => {
+            let button = this._buttons[this._getNextButtonToPlay()];
+            this.scene.beginAnimation(button, 0, 20, false, 1, resolve.bind(this));
+        });
+    }
+
+    private _getNextButtonToPlay() {
+        return this.sequence[this.runningSequenceIndex];
+    }
+
+    /**
+     * Adds a button to press to the sequence
+     */
+    private _addButton() {
+        let random = Math.floor(BABYLON.Scalar.RandomRange(0, 4));
+        this.sequence.push(random);
+    }
+
 }
